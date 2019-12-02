639806d2f57660f2336b95c98a0dce5ae67d4b4c
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-12-02 18:21:09 +0000

    feat: demo serializable transaction

--- database/connection.ts
+++ database/connection.ts
@@ -1,3 +1,3 @@
-import * as Knex from 'knex';
-const knexfile = require('../knexfile');
+import Knex from 'knex';
+import knexfile from '../knexfile';
 export const knex = Knex(knexfile.development);
--- server.ts
+++ server.ts
@@ -1,7 +1,33 @@
-import * as express from 'express';
+import express from 'express';
+import wait from 'waait';
 import { knex } from "./database/connection";
 
-(async () => {
+interface IAccount {
+  id: string;
+  status: string;
+}
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+async function initialiseDatabase() {
   await knex.migrate.latest();
   await knex.seed.run();
-})();
+}
+
+app.post('/approve-signups', async (req, res) => {
+  knex.transaction(async (trx: any) => {
+    const accounts = await knex<IAccount>('account')
+      .where({ status: 'AWAITING_APPROVAL' })
+      .update({ status: 'ACTIVE' })
+      .transacting(trx);
+    // simulate some long running process
+    await wait(5000);
+    return res.status(200).json({ accounts });
+  });
+});
+
+app.listen(port, async () => {
+  await initialiseDatabase();
+  console.log(`database-transactions server running on ${port}`);
+});
